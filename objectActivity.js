4b5c6aa07a31f7f2ffcc9fecc025078ffe9a1d96
clairepasamonte <clairepasamonte565@gmail.com>
2024-10-16 22:01:02 +0800

    Add files via upload

---
+++ objectActivity.js
@@ -0,0 +1,17 @@
+// Create an object called car with properties
+let car = {
+    type: "Sedan",
+    model: "Honda",
+    color: "Blue"
+};
+
+// Use typeof to check the type of object and log it
+console.log(typeof car);
+
+// Update the type property to "Toyota"
+car.type = "Toyota";
+console.log(car);
+
+// Add a new property wheels with the value 4
+car.wheels = 4;
+console.log(car);
