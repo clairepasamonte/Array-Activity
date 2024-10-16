6788458de21b9033f3d86cdb8893abb0488eb309
clairepasamonte <clairepasamonte565@gmail.com>
2024-10-16 21:58:47 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,15 @@
+// Create an array of favorite singers
+let favSingers = ['Dionela', 'Eminem', 'IU'];
+// Log the first singer
+console.log(favSingers[0]);
+
+// Create an array of favorite numbers
+let favNumbers = [7, 11, 21, 42];
+
+// Create an array with mixed data types
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+// Access and log each item in mixedArr
+for (let i = 0; i < mixedArr.length; i++) {
+    console.log(mixedArr[i]);
+}
